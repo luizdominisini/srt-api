1466933b0b146a149e9f6bdb18c38392e46f615c
Luiz Gustavo Dominisini <dominizi.luiz@gmail.com>
2024-12-17 10:14:56 -0300

    back finalizado

--- src/routes/routes-driver/routes-driver.service.ts
+++ src/routes/routes-driver/routes-driver.service.ts
@@ -4,5 +4,33 @@ import { PrismaService } from '../../prisma/prisma.service';
 @Injectable()
 export class RoutesDriverService {
   constructor(private readonly prismaService: PrismaService) {}
-  processRoute(dto: { route_id: string; lat: number; lng: number }) {}
+  processRoute(dto: { route_id: string; lat: number; lng: number }) {
+    return this.prismaService.routerDriver.upsert({
+      include: {
+        route: true,
+      },
+      where: { route_id: dto.route_id },
+      create: {
+        route_id: dto.route_id,
+        points: {
+          set: {
+            location: {
+              lat: dto.lat,
+              log: dto.lng,
+            },
+          },
+        },
+      },
+      update: {
+        points: {
+          push: {
+            location: {
+              lat: dto.lat,
+              log: dto.lng,
+            },
+          },
+        },
+      },
+    });
+  }
 }
